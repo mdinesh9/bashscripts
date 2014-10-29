60e9a35d6481ea9be3f528a4a71c96cc4353eab6
Dinesh M <dineshm.csit@gmail.com>
2014-10-29 19:56:19 +0530

    Run this program if you get vcvarshall.bat error while installing python packages in Windows 64bit machine #1

---
+++ vcvarshall.sh
@@ -0,0 +1,4 @@
+echo "Find vcvarshall.bat in your system and run it from this program for there."
+vcvarsall.bat x86_amd64
+set LIB=C:\OpenSSL-Win64\lib;%LIB%
+set INCLUDE=C:\OpenSSL-Win64\include;%INCLUDE%
